bdbb3a4c87c829f03dc0c0baac9d63bfa0b5fd12
Delly Mellas Nyman <nymandelly@gmail.com>
2024-11-21 22:16:50 +0700

    generate mock recon job finder service

--- bin/generate-mock.sh
+++ bin/generate-mock.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 for file in `find . -name '*.go' | grep service`; do
     if `grep -q 'interface {' ${file}`; then
         dest=${file//service\//}
-        mockgen -source=${file} -destination=test/mock/${dest}
+        mockgen -source=${file} -destination=test/mock/service/${dest}
     fi
 done
